a21813860f6486b6d0f6466e4e8c107492902db9
David Riedl <daves.weblab@gmail.com>
2020-09-21 21:38:53 +0200

    initial commit, testing

---
+++ src/theme.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import {init, hooks} from "mobro";
+
+// ----------------------------------------------------
+// component hook
+
+// hooks.component("entry", (Component) => (props) => (
+//     <div className="my-wrapper">
+//         <Component {...props}/>
+//     </div>
+// ));
+
+// ----------------------------------------------------
+// reducers hook
+
+// const hardwareInitialState = {
+//     hardware: "intel"
+// }
+//
+// const dataInitialState = {
+//     data: "stuff"
+// }
+//
+// hooks.redux.reducers(event => event.mergeReducers({
+//     theme: event.combineReducers({
+//         hardware: event.createReducer(hardwareInitialState, {}),
+//         data: event.createReducer(dataInitialState, {})
+//     })
+// }));
+
+// ----------------------------------------------------
+// map state to props hook
+
+// hooks.redux.mapStateToProps("app", event => event.mergeMapStateToProps({
+//     myCustomPropFromState: event.state.layout.foo,
+//     hardware: event.state.theme.hardware.hardware,
+//     data: event.state.theme.data.data
+// }));
+
+init();
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require("path");
+
+module.exports = {
+    devtool: "source-map",
+    watch: true,
+
+    entry: {
+        theme: path.resolve("src/theme.jsx")
+    },
+
+    output: {
+        path: path.resolve(__dirname, "../dist"),
+        filename: "[name].js"
+    },
+
+    resolve: {
+        modules: [
+            path.resolve(__dirname, "src"),
+            path.resolve(__dirname, "node_modules")
+        ],
+
+        extensions: ["*", ".js", ".jsx"]
+    },
+
+    externals: {
+        react: "React",
+        mobro: "mobro"
+    },
+
+    module: {
+        rules: [{
+            test: /\.(js|jsx)$/,
+            exclude: /(node_modules)/,
+            use: {
+                loader: "babel-loader",
+                options: {
+                    plugins: ['@babel/plugin-transform-runtime', '@babel/plugin-proposal-class-properties'],
+                    presets: ["@babel/preset-env", "@babel/react"]
+                }
+            }
+        }, {
+            test: /\.scss$/,
+            use: ["style-loader", "css-loader", "sass-loader"]
+        }]
+    }
+};
